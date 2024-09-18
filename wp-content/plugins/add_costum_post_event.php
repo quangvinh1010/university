2fef32698acd7d29aa9a8f38a35635e7eb15c66e
quangvinh1010 <vinhq1813@gmail.com>
2024-09-18 07:35:38 +0700

    first commit

---
+++ wp-content/plugins/add_costum_post_event.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Plugin Name: Event Custom Post Type
+Description: Adds a custom post type for events.
+Author: Qvinh
+Version: 1.0
+*/
+
+function university_post_types() {
+    register_post_type('event', array(
+      'public' => true, 
+      'supports' => array('thumbnail', 'title', 'editor'),
+      'labels' => array(  
+        'name' => 'Events', 
+        'add_new_item' => 'Add New Event', 
+        'edit_item' => 'Edit Event', 
+        'all_items' => 'All Events', 
+        'singular_name' => 'Event'  
+      ),  
+        'menu_icon' => 'dashicons-images-alt2'
+    ));
+    }  
+  add_action('init', 'university_post_types');
